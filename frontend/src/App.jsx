52397b0e642ad2c970ad2292697cca9ccee7130d
Muhammad Arafat Azam <arafatazam17@gmail.com>
2021-07-11 05:30:08 +0600

    React router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import MakeReservation from "./pages/MakeReservation";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
-  return <h1>Hello World</h1>;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <MakeReservation />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
-export default App;
---
+++ frontend/src/pages/MakeReservation.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MakeReservation() {
+  return <h1>Reservation Page</h1>;
+}
