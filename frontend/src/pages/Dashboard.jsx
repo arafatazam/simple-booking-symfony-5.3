e2df20f24f2955416c01ef0708867d40e19ae49a
Muhammad Arafat Azam <arafatazam17@gmail.com>
2021-07-12 22:14:32 +0600

    Dashboard Calendar UI

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,122 @@
-import React from "react";
-import { Calendar } from "antd";
+import React, { useState, useRef } from "react";
+import {
+  Calendar,
+  Modal,
+  Spin,
+  Typography,
+  Form,
+  Button,
+  InputNumber,
+  notification,
+} from "antd";
+
+const { Text } = Typography;
 
 export default function Dashboard() {
-  return <Calendar></Calendar>;
+  const [isSpinning, setSpinning] = useState(false);
+  const [bookingData, setBookingData] = useState({});
+  const [form] = Form.useForm();
+  const selecTed = useRef(null);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalBusy, setModalBusy] = useState(false);
+
+  const onPanelChange = (date, mode) => {
+    if (mode == "year") {
+      return;
+    }
+    setSpinning(true);
+    setTimeout(() => {
+      setBookingData({ "2021-07-15": { available: 12, booked: 10 } });
+      setSpinning(false);
+    }, 1500);
+  };
+
+  const renderDateCell = (date) => {
+    let key = date.format("YYYY-MM-DD");
+    if (!bookingData[key]) {
+      return <Text type="secondary">Click To Create Vacancy</Text>;
+    }
+    return (
+      <>
+        <p>
+          <Text>Availabe - {bookingData[key]["available"]}</Text>
+        </p>
+        <p>
+          <Text type="warning" mark>
+            Booked - {bookingData[key]["booked"]}
+          </Text>
+        </p>
+      </>
+    );
+  };
+
+  const showModal = (date) => {
+    form.resetFields();
+    form.setFieldsValue({
+      slotsAvailable: 12,
+      price: 10.5,
+    });
+    setModalVisible(true);
+  };
+
+  const onDateChange = (date) => {
+    let prev = selecTed.current;
+    selecTed.current = date;
+    if (
+      prev != null &&
+      (prev.year() != date.year() || prev.month() != date.month())
+    ) {
+      return;
+    }
+    showModal(date);
+  };
+  return (
+    <>
+      <Spin spinning={isSpinning} size="large">
+        <Calendar
+          onPanelChange={onPanelChange}
+          onSelect={onDateChange}
+          dateCellRender={renderDateCell}
+        ></Calendar>
+      </Spin>
+      <Modal
+        visible={modalVisible}
+        footer={null}
+        onCancel={() => setModalVisible(false)}
+      >
+        <Spin spinning={modalBusy}>
+          <Form
+            form={form}
+            style={{ marginTop: "30px" }}
+            onFinishFailed={(errorInfo) =>
+              notification.open({ type: "error", message: errorInfo })
+            }
+            onFinish={(values) => {
+              console.log(values);
+              setModalVisible(false);
+            }}
+          >
+            <Form.Item
+              name="slotsAvailable"
+              rules={[{ required: true, type: "integer", min: 1 }]}
+              label="Available Slots"
+            >
+              <InputNumber min={1} placeholder="Input number of slots" />
+            </Form.Item>
+            <Form.Item
+              name="price"
+              rules={[{ required: true, type: "float", min: 0 }]}
+              label="Price"
+            >
+              <InputNumber placeholder="Input price" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">Submit</Button>
+            </Form.Item>
+          </Form>
+        </Spin>
+      </Modal>
+    </>
+  );
 }
--- frontend/src/pages/MakeReservation.jsx
+++ frontend/src/pages/MakeReservation.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-import { Form, DatePicker, Button } from "antd";
+import React, { useState } from "react";
+import { Form, DatePicker, Button, Spin } from "antd";
 
 export default function MakeReservation() {
+  let [isSpinning, setSpinning] = useState(false);
   return (
-    <Form>
-      <Form.Item>
-        <DatePicker.RangePicker />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Make Reservation
-        </Button>
-      </Form.Item>
-    </Form>
+    <Spin spinning={isSpinning}>
+      <Form>
+        <Form.Item>
+          <DatePicker.RangePicker />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Make Reservation
+          </Button>
+        </Form.Item>
+      </Form>
+    </Spin>
   );
 }
